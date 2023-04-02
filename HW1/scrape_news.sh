db204205e5ecfff6daf87c4ec2eb6957f7e1f592
Mamat <beneldokow@gmail.com>
2023-04-02 19:31:04 +0300

    first final vs

--- HW1/scrape_news.sh
+++ HW1/scrape_news.sh
@@ -7,18 +7,30 @@ https://www.ynetnews.com/category/3082 2>/dev/null |\
 
 list=$(echo "$articles"| tr '\n' '\0' | sort -uz | tr '\0' '\n')
 echo $(echo "$list" | wc -l)
+echo
 
 for url in $list; do
     echo -n "$url, "
-    for name in Netanyahu Gantz Bennett; do
-        data=$(wget --no-check-certificate -O - $url 2>/dev/null)  
-        echo -n "$name, "
-        echo "$data" | grep -c $name | tr -d '\n'
-        echo -n ", "
+    count=0
+    
+    for name in Netanyahu Gantz Bennett Peretz; do
+        data=$(wget --no-check-certificate -O - $url 2>/dev/null)
+        count+=$(echo "$data" | grep -c $name)
     done
-    name=Peretz
-    data=$(wget --no-check-certificate -O - $url 2>/dev/null)
-        echo -n "$name, "
-        echo "$data" | grep -c $name
-        echo
+    
+    if [ $count -eq 0 ]; then
+        echo "-"
+    else
+        for name in Netanyahu Gantz Bennett Peretz; do
+            data=$(wget --no-check-certificate -O - $url 2>/dev/null)
+            echo -n "$name, "
+            num=$(echo "$data" | grep -c $name)
+            if [ $name = "Peretz" ]; then
+                echo "$num"
+            else
+                echo -n "$num, "
+            fi
+        done
+    fi
+    echo
 done
