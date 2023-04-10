277300db7ef0415832f20eadd91402b635b3bcb0
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 <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>
2023-04-10 13:40:32 +0300

    HW1 FinalVersion

--- HW1/scrape_news.sh
+++ HW1/scrape_news.sh
@@ -7,7 +7,6 @@ https://www.ynetnews.com/category/3082 2>/dev/null |\
 
 list=$(echo "$articles"| tr '\n' '\0' | sort -uz | tr '\0' '\n')
 echo $(echo "$list" | wc -l)
-echo
 
 for url in $list; do
     echo -n "$url, "
@@ -15,7 +14,7 @@ for url in $list; do
     
     for name in Netanyahu Gantz Bennett Peretz; do
         data=$(wget --no-check-certificate -O - $url 2>/dev/null)
-        count+=$(echo "$data" | grep -c $name)
+        count+=$(echo "$data" | grep -cP $name)
     done
     
     if [ $count -eq 0 ]; then
@@ -24,7 +23,7 @@ for url in $list; do
         for name in Netanyahu Gantz Bennett Peretz; do
             data=$(wget --no-check-certificate -O - $url 2>/dev/null)
             echo -n "$name, "
-            num=$(echo "$data" | grep -c $name)
+            num=$(echo "$data" | grep -cP $name)
             if [ $name = "Peretz" ]; then
                 echo "$num"
             else
@@ -32,5 +31,4 @@ for url in $list; do
             fi
         done
     fi
-    echo
 done
